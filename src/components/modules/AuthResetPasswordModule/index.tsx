f80a7019d5966af295b0bdc9524a7ebd5083806a
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-14 22:09:27 +0700

    feat: verification page ui done

--- src/components/modules/AuthResetPasswordModule/index.tsx
+++ src/components/modules/AuthResetPasswordModule/index.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@chakra-ui/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
@@ -5,8 +6,13 @@ import { SWButton } from "src/components/elements/Button";
 import { ResetPasswordModuleProps } from "./interface";
 
 export const AuthResetPasswordModule: React.FC = () => {
+  const [selectedAccount, setSelectedAccount] = useState<string>("");
   const router = useRouter();
   const [page, setPage] = useState(1);
+
+  const selectAccount = (account: string) => {
+    setSelectedAccount(account);
+  };
 
   const nextPage = () => {
     setPage(page + 1);
@@ -20,9 +26,21 @@ export const AuthResetPasswordModule: React.FC = () => {
     case 0:
       router.push("/auth/login");
     case 1:
-      return <SendVerificationPage prevPage={prevPage} nextPage={nextPage} />;
+      return (
+        <SendVerificationPage
+          prevPage={prevPage}
+          nextPage={nextPage}
+          selectAccount={selectAccount}
+        />
+      );
     case 2:
-      return <VerificationPage prevPage={prevPage} nextPage={nextPage} />;
+      return (
+        <VerificationPage
+          prevPage={prevPage}
+          nextPage={nextPage}
+          verificationSendTo={selectedAccount}
+        />
+      );
     case 3:
       return <ResetPasswordPage prevPage={prevPage} nextPage={nextPage} />;
     case 4:
@@ -37,7 +55,17 @@ export const AuthResetPasswordModule: React.FC = () => {
 const SendVerificationPage = ({
   prevPage,
   nextPage,
+  selectAccount,
 }: ResetPasswordModuleProps) => {
+  const verifyViaSMS = () => {
+    selectAccount("XXXX XXXX 4235");
+    nextPage();
+  };
+
+  const verifyViaEmail = () => {
+    selectAccount("XXXX XXXX ms@gmail.com");
+    nextPage();
+  };
   return (
     <div>
       <div className="px-8 py-8 h-screen relative">
@@ -56,7 +84,10 @@ const SendVerificationPage = ({
           Select which contact details should we use to reset your password
         </div>
         {/* TODO: change to dynamic info of user credentials */}
-        <div className="flex gap-3 bg-[#252525]/70 py-4 px-6 rounded-2xl mt-8">
+        <div
+          className="flex gap-3 bg-[#252525]/70 py-4 px-6 rounded-2xl mt-8"
+          onClick={verifyViaSMS}
+        >
           <Image
             src="/assets/icons/message.svg"
             alt="message"
@@ -68,7 +99,10 @@ const SendVerificationPage = ({
             <div className="text-white">XXXX XXXX 4235</div>
           </div>
         </div>
-        <div className="flex gap-3 bg-[#252525]/70 py-4 px-6 rounded-2xl mt-4">
+        <div
+          className="flex gap-3 bg-[#252525]/70 py-4 px-6 rounded-2xl mt-4"
+          onClick={verifyViaEmail}
+        >
           <Image
             src="/assets/icons/mail-2.svg"
             alt="message"
@@ -80,16 +114,90 @@ const SendVerificationPage = ({
             <div className="text-white">XXXX XXXX ms@gmail.com</div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+
+const VerificationPage = ({
+  prevPage,
+  nextPage,
+  verificationSendTo,
+}: ResetPasswordModuleProps) => {
+  return (
+    <div>
+      <div className="px-8 py-8 h-screen relative">
+        <div onClick={prevPage}>
+          <Image
+            src="/assets/icons/back.svg"
+            alt="landing"
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className="text-white text-3xl font-bold py-4">
+          Enter 4-digit Verification code
+        </div>
+        <div className="text-white text-sm font-light">
+          Code send to {verificationSendTo}. This code will expired in 01:30
+        </div>
+        {/* TODO: change to dynamic info of user credentials */}
+        <div className="flex gap-3 py-4 px-6 rounded-2xl mt-8">
+          <Input
+            placeholder=""
+            backgroundColor={"#252525"}
+            opacity={0.8}
+            border="none"
+            borderRadius={12}
+            textAlign="center"
+            textColor={"#fff"}
+            fontSize={24}
+            fontWeight={600}
+            size="lg"
+          />
+          <Input
+            placeholder=""
+            backgroundColor={"#252525"}
+            opacity={0.8}
+            border="none"
+            borderRadius={12}
+            textAlign="center"
+            textColor={"#fff"}
+            fontSize={24}
+            fontWeight={600}
+            size="lg"
+          />
+          <Input
+            placeholder=""
+            backgroundColor={"#252525"}
+            opacity={0.8}
+            border="none"
+            borderRadius={12}
+            textAlign="center"
+            textColor={"#fff"}
+            fontSize={24}
+            fontWeight={600}
+            size="lg"
+          />
+          <Input
+            placeholder=""
+            backgroundColor={"#252525"}
+            opacity={0.8}
+            border="none"
+            borderRadius={12}
+            textAlign="center"
+            textColor={"#fff"}
+            fontSize={24}
+            fontWeight={600}
+            size="lg"
+          />
+        </div>
         <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
           <SWButton label="Next" onClick={nextPage} />
         </div>
       </div>
     </div>
   );
-};
-
-const VerificationPage = ({ prevPage, nextPage }: ResetPasswordModuleProps) => {
-  return <div></div>;
 };
 
 const ResetPasswordPage = ({
--- src/components/modules/AuthResetPasswordModule/interface.ts
+++ src/components/modules/AuthResetPasswordModule/interface.ts
@@ -1,4 +1,6 @@
 export interface ResetPasswordModuleProps {
   prevPage: () => void;
   nextPage: () => void;
+  selectAccount?: any;
+  verificationSendTo?: string;
 }
