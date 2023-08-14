759eb4a28908a7b527d37b0761179bafe6ceb084
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-14 21:29:02 +0700

    feat: reset password skeleton

--- src/components/modules/AuthResetPasswordModule/index.tsx
+++ src/components/modules/AuthResetPasswordModule/index.tsx
@@ -1,3 +1,54 @@
+import React, { useState } from "react";
+import { ResetPasswordModuleProps } from "./interface";
+
 export const AuthResetPasswordModule: React.FC = () => {
+  const [page, setPage] = useState(1);
+
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    setPage(page - 1);
+  };
+
+  switch (page) {
+    case 1:
+      return <SendVerificationPage prevPage={prevPage} nextPage={nextPage} />;
+    case 2:
+      return <VerificationPage prevPage={prevPage} nextPage={nextPage} />;
+    case 3:
+      return <ResetPasswordPage prevPage={prevPage} nextPage={nextPage} />;
+    case 4:
+      return (
+        <ResetPasswordSuccessPage prevPage={prevPage} nextPage={nextPage} />
+      );
+    default:
+      return <SendVerificationPage prevPage={prevPage} nextPage={nextPage} />;
+  }
+};
+
+const SendVerificationPage = ({
+  prevPage,
+  nextPage,
+}: ResetPasswordModuleProps) => {
+  return <div></div>;
+};
+
+const VerificationPage = ({ prevPage, nextPage }: ResetPasswordModuleProps) => {
+  return <div></div>;
+};
+
+const ResetPasswordPage = ({
+  prevPage,
+  nextPage,
+}: ResetPasswordModuleProps) => {
+  return <div></div>;
+};
+
+const ResetPasswordSuccessPage = ({
+  prevPage,
+  nextPage,
+}: ResetPasswordModuleProps) => {
   return <div></div>;
 };
--- src/components/modules/AuthResetPasswordModule/interface.ts
+++ src/components/modules/AuthResetPasswordModule/interface.ts
@@ -1,1 +1,4 @@
-export interface Props {}
+export interface ResetPasswordModuleProps {
+  prevPage: () => void;
+  nextPage: () => void;
+}
