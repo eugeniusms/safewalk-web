aae9ada58e8e5898bc6e5c83380667799cb81722
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-14 23:23:42 +0700

    feat: set navbar active assets

--- src/components/elements/Navbar/index.tsx
+++ src/components/elements/Navbar/index.tsx
@@ -5,24 +5,31 @@ import { BsMegaphone } from "react-icons/bs";
 
 const Navbar = () => {
   const router = useRouter();
+  console.log(router.pathname);
+
+  let iconMenuHome = "/assets/icons/menu-home.svg";
+  let iconMenuLocation = "/assets/icons/menu-location.svg";
+  let iconMenuCall = "/assets/icons/menu-call.svg";
+  let iconMenuProfile = "/assets/icons/menu-profile.svg";
+
+  if (router.pathname === "/") {
+    iconMenuHome = "/assets/icons/menu-home-active.svg";
+  } else if (router.pathname === "/maps") {
+    iconMenuLocation = "/assets/icons/menu-location-active.svg";
+  } else if (router.pathname === "/contact/list") {
+    iconMenuCall = "/assets/icons/menu-call-active.svg";
+  } else if (router.pathname === "/profile") {
+    iconMenuProfile = "/assets/icons/menu-profile-active.svg";
+  }
+
   return (
     <div className="flex justify-between bg-[#252525] absolute bottom-0 left-0 right-0 items-center py-2 px-10 rounded-t-3xl">
       <div onClick={() => router.push("/")}>
-        <Image
-          src="/assets/icons/menu-home.svg"
-          alt="logo"
-          width={20}
-          height={20}
-        />
+        <Image src={iconMenuHome} alt="logo" width={20} height={20} />
       </div>
       <Spacer />
       <div onClick={() => router.push("/maps")}>
-        <Image
-          src="/assets/icons/menu-location.svg"
-          alt="logo"
-          width={20}
-          height={20}
-        />
+        <Image src={iconMenuLocation} alt="logo" width={20} height={20} />
       </div>
       <Spacer />
       {/* TODO: routing on megaphone menu */}
@@ -31,21 +38,11 @@ const Navbar = () => {
       </div>
       <Spacer />
       <div onClick={() => router.push("/contact/list")}>
-        <Image
-          src="/assets/icons/menu-call.svg"
-          alt="logo"
-          width={20}
-          height={20}
-        />
+        <Image src={iconMenuCall} alt="logo" width={20} height={20} />
       </div>
       <Spacer />
       <div onClick={() => router.push("/profile")}>
-        <Image
-          src="/assets/icons/menu-profile.svg"
-          alt="logo"
-          width={20}
-          height={20}
-        />
+        <Image src={iconMenuProfile} alt="logo" width={20} height={20} />
       </div>
     </div>
   );
