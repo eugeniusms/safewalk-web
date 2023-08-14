5171729353c8c227d931ca0b1635d984c41a8c25
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-14 17:13:45 +0700

    feat: register success page done

--- src/components/modules/AuthRegisterModule/index.tsx
+++ src/components/modules/AuthRegisterModule/index.tsx
@@ -525,5 +525,29 @@ const RegisterPhotoSuccess = ({ prevPage, nextPage }: RegisterModuleProps) => {
 };
 
 const RegisterSuccess = ({ prevPage, nextPage }: RegisterModuleProps) => {
-  return <div></div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center relative">
+      <div>
+        <div className="flex justify-center">
+          <Image
+            src="/assets/images/success.svg"
+            alt="landing"
+            width={160}
+            height={160}
+          />
+        </div>
+        <div className="pt-4 pb-10">
+          <div className="text-center text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#4D61A3] to-[#3E35F7]">
+            Congrats!
+          </div>
+          <div className="text-center text-xl text-white text-base font-semibold py-2">
+            Your Profile Is Ready To Use
+          </div>
+        </div>
+        <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
+          <SWButton label="Next" onClick={nextPage} />
+        </div>
+      </div>
+    </div>
+  );
 };
