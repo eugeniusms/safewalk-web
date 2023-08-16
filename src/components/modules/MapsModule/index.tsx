5678bb232e52b55b0ad9b464a95d7fde2dc43bb9
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-16 16:40:27 +0700

    setup: react google maps api installed

--- src/components/modules/MapsModule/index.tsx
+++ src/components/modules/MapsModule/index.tsx
@@ -1,9 +1,52 @@
-import Layout from "src/components/elements/Layout";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useCallback, useEffect, useState } from "react";
+import map_setup from "src/services/map_setup_data.json";
 
 export const MapsModule: React.FC = () => {
-  return (
-    <Layout>
-      <div className="h-[92vh]">TEST</div>
-    </Layout>
+  const [map, setMap] = useState(null);
+  const [zoom, setZoom] = useState(14.6);
+  const [center, setCenter] = useState(map_setup.center);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "b2aff9ed92cab97a",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
+  });
+
+  const onLoad = useCallback(function callback(map: any) {
+    const bounds = new window.google.maps.LatLngBounds(map_setup.center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setZoom(14.7);
+    }, 300);
+  }, []);
+
+  const clickModalHandler = (center: any) => {
+    setZoom(18);
+  };
+
+  console.log("CENTER: ", center);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={map_setup.container_style}
+      center={center}
+      zoom={zoom}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={map_setup.map_id}
+    >
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
   );
 };
