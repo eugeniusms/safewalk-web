61cefd91f8409d138f73f9bc651ed3cdd46b8547
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-30 20:22:08 +0700

    feat: create contact fe done

--- src/components/modules/ContactCreateModule/index.tsx
+++ src/components/modules/ContactCreateModule/index.tsx
@@ -5,12 +5,16 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { Controller, useForm } from "react-hook-form";
 import { SWButton } from "src/components/elements/Button";
 import { Layout } from "src/components/elements/Layout";
+import UploadPhoto from "src/components/elements/Upload";
 import { useLocalStorage } from "src/components/hooks/useLocalStorage";
+import { storage } from "src/services/firebase";
+import { v4 } from "uuid";
 import { FormDefault } from "./interface";
 
 export const ContactCreateModule = () => {
@@ -67,6 +71,21 @@ export const ContactCreateModule = () => {
           isClosable: true,
         });
       });
+    router.push("/contact/list");
+  };
+
+  // Upload to firebase storage
+  const handleUpload = (file: File) => {
+    if (file == null) {
+      return;
+    }
+    const imageRef = ref(storage, `images/contact/${file.name + v4()}`);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        console.log("File available at", url);
+        setValue("photo_url", url);
+      });
+    });
   };
 
   return (
@@ -84,12 +103,25 @@ export const ContactCreateModule = () => {
           Emergency Contact
         </div>
         <div className="flex justify-center">
-          <div className="w-full py-8">
+          <div className="w-full py-4">
             <form
               onSubmit={handleSubmit(onSubmit)}
               className="flex flex-col gap-4 text-white"
             >
               <div>
+                <div className="pb-8">
+                  <UploadPhoto
+                    onUpload={handleUpload}
+                    trigger={
+                      <Image
+                        src="/assets/icons/upload-photo.svg"
+                        alt="gallery-button"
+                        width={180}
+                        height={180}
+                      />
+                    }
+                  />
+                </div>
                 <Controller
                   name="name"
                   control={control}
@@ -194,7 +226,7 @@ export const ContactCreateModule = () => {
         </div>
         <div className="flex justify-center pt-8 gap-2">
           <button
-            className="bg-[#F4F4F4]/10 text-[#3E35F7] font-bold rounded-xl px-16"
+            className="bg-[#F4F4F4]/10 text-[#3E35F7] font-bold rounded-xl px-16 py-2"
             onClick={() => router.push("/contact/list")}
           >
             Cancel
